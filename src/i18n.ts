44395dc8e62d5698cf475e5f803d57f587ecfb4e
sum117 <jvcaliman@hotmail.com>
2024-03-06 19:12:58 -0300

    fix: Reorder i18next.addResourceBundle calls in i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,12 +2,12 @@ import i18next, { type TOptions } from "i18next";
 import enUS from "./locales/en-US.json";
 import ptBr from "./locales/pt-BR.json";
 
-i18next.addResourceBundle("en-US", "locales", enUS, true, true);
-i18next.addResourceBundle("pt-BR", "locales", ptBr, true, true);
 i18next.init({
   ns: ["locales"],
   defaultNS: "locales",
 });
+i18next.addResourceBundle("en-US", "locales", enUS, true, true);
+i18next.addResourceBundle("pt-BR", "locales", ptBr, true, true);
 
 export type TranslationKey = keyof typeof ptBr | keyof typeof enUS;
 
