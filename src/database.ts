906b67faebc06f54c26e65c19ecaf8a602e246b8
sum117 <jvcaliman@hotmail.com>
2024-03-06 19:13:12 -0300

    feat: Update database.ts to import schema and pass it to drizzle

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,10 @@
-import { Database } from "bun:sqlite";
+import Database from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
+import * as schema from "./schema";
 
 const sqlite = new Database("sqlite.db");
-const db = drizzle(sqlite);
+const db = drizzle(sqlite, { schema });
 migrate(db, { migrationsFolder: "drizzle" });
 
 export default db;
