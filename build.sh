e15697608ab5be16b61f295ca00b72cee59be514
sum117 <jvcaliman@hotmail.com>
2024-04-20 16:13:56 -0300

    chore: local build script for my convenience

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@ bun run build
 cp -r drizzle build/
 cp sqlite.db build/
 cp package.json build/
+tar -czvf build.tar.gz build
+scp ./build.tar.gz character-vault:/home/sum117/projects/character-vault
+ssh character-vault -t 'cd ~/projects/character-vault && tar -xzvf build.tar.gz && cp -rf build/* . && rm -rf build.tar.gz && rm -rf build && sudo systemctl restart character-vault.service'
+rm -rf ./build.tar.gz
