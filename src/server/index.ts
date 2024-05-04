89fee50a3801a2b76cefbacaff2481a3a5a787b8
sum117 <jvcaliman@hotmail.com>
2024-05-04 02:21:21 -0300

    fix: add bodyLimit middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { bodyLimit } from "hono/body-limit";
 import { DateTime } from "luxon";
 
 import CharacterService from "@/services/characterService";
@@ -61,51 +62,60 @@ api.patch("/characters/set-active/:userId/:characterId", async (context) => {
   });
 });
 
-api.post("/characters/create/:userId", async (context) => {
-  const userId = context.req.param("userId");
-  if (!userId || !matchDiscordId.test(userId)) {
-    return context.json({ ok: false, error: "Invalid user id" }, 400);
-  }
-  const characterFormData = await context.req.parseBody<{
-    name: string;
-    image: File;
-    title: string;
-    embedColor: string;
-  }>();
-  const allowedTypes = ["image/png", "image/jpeg", "image/jpg"];
-  if (!(characterFormData.image instanceof File)) {
-    return context.json({ ok: false, error: "Image is required" }, 400);
-  }
-  const isImage = (image: File): image is File & { type: string } => {
-    return "type" in image && typeof image.type === "string" && allowedTypes.includes(image.type);
-  };
-  if (!isImage(characterFormData.image)) {
-    return context.json({ ok: false, error: "Invalid image type" }, 400);
-  }
-
-  const formData = await context.req.formData();
-  formData.delete("name");
-  formData.delete("embedColor");
-  formData.append("image", characterFormData.image);
-  formData.append("type", "image");
-  formData.append("title", characterFormData.name + " - " + DateTime.now().toISO());
-
-  console.log(formData);
-
-  const imgurResponse = await fetch("https://api.imgur.com/3/image", {
-    method: "POST",
-    headers: {
-      Authorization: "Client-ID " + Bun.env.IMGUR_CLIENT_ID,
+api.post(
+  "/characters/create/:userId",
+  bodyLimit({
+    maxSize: 50 * 1024 * 1024,
+    onError: (context) => {
+      context.json({ ok: false, error: "File too large" }, 413);
     },
-    body: formData,
-  });
-  const imgurJson = await imgurResponse.json();
-  if (!imgurJson.success) {
-    return context.json({ ok: false, error: "Error uploading image" }, 500);
-  }
-  const characterToCreate = { ...characterFormData, imageUrl: imgurJson.data.link };
-  const newCharacter = await CharacterService.createCharacter({ authorId: userId, ...characterToCreate });
-  return context.json({ ok: true, character: newCharacter }, 201);
-});
+  }),
+  async (context) => {
+    const userId = context.req.param("userId");
+    if (!userId || !matchDiscordId.test(userId)) {
+      return context.json({ ok: false, error: "Invalid user id" }, 400);
+    }
+    const characterFormData = await context.req.parseBody<{
+      name: string;
+      image: File;
+      title: string;
+      embedColor: string;
+    }>({});
+    const allowedTypes = ["image/png", "image/jpeg", "image/jpg"];
+    if (!(characterFormData.image instanceof File)) {
+      return context.json({ ok: false, error: "Image is required" }, 400);
+    }
+    const isImage = (image: File): image is File & { type: string } => {
+      return "type" in image && typeof image.type === "string" && allowedTypes.includes(image.type);
+    };
+    if (!isImage(characterFormData.image)) {
+      return context.json({ ok: false, error: "Invalid image type" }, 400);
+    }
+
+    const formData = await context.req.formData();
+    formData.delete("name");
+    formData.delete("embedColor");
+    formData.append("image", characterFormData.image);
+    formData.append("type", "image");
+    formData.append("title", characterFormData.name + " - " + DateTime.now().toISO());
+
+    console.log(formData);
+
+    const imgurResponse = await fetch("https://api.imgur.com/3/image", {
+      method: "POST",
+      headers: {
+        Authorization: "Client-ID " + Bun.env.IMGUR_CLIENT_ID,
+      },
+      body: formData,
+    });
+    const imgurJson = await imgurResponse.json();
+    if (!imgurJson.success) {
+      return context.json({ ok: false, error: "Error uploading image" }, 500);
+    }
+    const characterToCreate = { ...characterFormData, imageUrl: imgurJson.data.link };
+    const newCharacter = await CharacterService.createCharacter({ authorId: userId, ...characterToCreate });
+    return context.json({ ok: true, character: newCharacter }, 201);
+  },
+);
 
 export default api;
