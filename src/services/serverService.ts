8f5674ffb71a7e089c96ed4a682c271b406ffbbe
sum117 <jvcaliman@hotmail.com>
2025-05-12 21:24:14 -0300

    feat: Add user server data management methods for creating and updating user server data

--- src/services/serverService.ts
+++ src/services/serverService.ts
@@ -2,7 +2,7 @@ import { eq } from "drizzle-orm";
 
 import db from "../database";
 import Server from "../models/Server";
-import { characterServerData, servers } from "../schema";
+import { characterServerData, servers, userServerData } from "../schema";
 
 export type ServerType = typeof servers.$inferSelect;
 export class ServerService {
@@ -36,7 +36,37 @@ export class ServerService {
     }
     return serverData;
   }
+  public static async getOrCreateUserServerData(userId: string, serverId: string) {
+    const serverData = await db.query.userServerData.findFirst({
+      where: (data, { and }) => and(eq(data.userId, userId), eq(data.serverId, serverId)),
+    });
+    if (!serverData) {
+      const [createdServerData] = await db.insert(userServerData).values({ userId, serverId, streak: 0 }).returning();
+      if (!createdServerData) {
+        throw new Error(`Failed to create userServerData for user ${userId} and server ${serverId}`);
+      }
+      return createdServerData;
+    }
+    return serverData;
+  }
+  public static async updateUserServerData(
+    userId: string,
+    serverId: string,
+    values: {
+      streak?: number;
+    },
+  ) {
+    const [updatedServerData] = await db
+      .update(userServerData)
+      .set({ streak: values.streak, lastStreakAt: new Date() })
+      .where(eq(userServerData.userId, userId))
+      .returning();
 
+    if (!updatedServerData) {
+      throw new Error(`Failed to update userServerData for user ${userId} and server ${serverId}`);
+    }
+    return updatedServerData;
+  }
   public static updateServer(server: Server) {
     return db.update(servers).set(server).where(eq(servers.id, server.id)).returning();
   }
